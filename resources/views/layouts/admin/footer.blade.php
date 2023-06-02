d11fe7b208eee3d47419b6400532e812cad35cb8
Muhammad Faizan Khalil <faizan.khalil@oladoc.com>
2023-06-02 23:25:07 +0500

    Initial commit

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,11 @@
+<!--**********************************
+            Footer start
+        ***********************************-->
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Developed by <a href="https://theowllogics.com/" target="_blank">Owl Logics</a> {{date('Y')}}</p>
+    </div>
+</div>
+<!--**********************************
+    Footer end
+***********************************-->
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin',[LoginController::class,'showAdminLoginForm'])->name('admin.login-view');
+Route::post('/admin',[LoginController::class,'adminLogin'])->name('admin.login');
+
+Route::get('/admin/register',[RegisterController::class,'showAdminRegisterForm'])->name('admin.register-view');
+Route::post('/admin/register',[RegisterController::class,'createAdmin'])->name('admin.register');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
